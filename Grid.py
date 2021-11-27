fdc60150762d8e1818a7b359be947cd27ba67f17
thakira <50866858+thakira@users.noreply.github.com>
2021-11-27 12:41:56 +0100

    PEP8 ok

--- Grid.py
+++ Grid.py
@@ -1,6 +1,6 @@
 # Kästchenmuster für Spielbrett erstellen
 import pygame as pg
-from Settings import BOARD_SIZE, CELL_SIZE, GRID_DIMENSION, WHITE
+from Settings import BOARD_SIZE, CELL_SIZE, GRID_DIMENSION, WHITE, BLACK
 
 
 class Grid:
@@ -10,8 +10,8 @@ class Grid:
         self.dimension = GRID_DIMENSION
         self.cell_size = int(CELL_SIZE)
         self.surface = pg.Surface((self.grid_size, self.grid_size))
-        self.surface.set_alpha(180)
-        self.surface.fill((155, 181, 196))
+        # self.surface.set_alpha(150)
+        self.surface.fill(WHITE)
 
         # Liste für Zelleninhalte erstellen
         self.cells = []
@@ -20,14 +20,12 @@ class Grid:
             for self.column in range(self.dimension):
                 self.cells[self.row].append(None)
 
-
         # Hintergrund und Zwischenlinien zeichnen
         for x in range(0, self.grid_size, self.cell_size):
-            pg.draw.lines(self.surface, WHITE, False,
+            pg.draw.lines(self.surface, BLACK, False,
                           [[0, x], [self.grid_size, x]], 1)
-            pg.draw.lines(self.surface, WHITE, False,
+            pg.draw.lines(self.surface, BLACK, False,
                           [[x, 0], [x, self.grid_size]], 1)
-
 
     def get_grid_surface(self):
         return self.surface
@@ -39,11 +37,8 @@ class Grid:
     def get_grid_coords(self, pos):
         column = pos[0] // self.cell_size
         row = pos[1] // self.cell_size
-        coords = (row , column)
-        x = column * (self.cell_size)
-        y = row * (self.cell_size)
+        coords = (row, column)
+        x = column * self.cell_size
+        y = row * self.cell_size
         cellcenter = (x, y)
         return coords, cellcenter
-
-
-
