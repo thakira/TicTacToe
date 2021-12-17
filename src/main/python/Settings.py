3d6a0402832a33cec418512bd4151f6138a72e68
thakira <50866858+thakira@users.noreply.github.com>
2021-12-17 22:00:42 +0100

    changed path to pictures

--- src/main/python/Settings.py
+++ src/main/python/Settings.py
@@ -11,8 +11,10 @@ CELL_SIZE = int(BOARD_SIZE // GRID_DIMENSION)
 BOARD_HEIGHT = int(BOARD_SIZE + CELL_SIZE)
 
 SYMBOL_SIZE = int(CELL_SIZE * 0.75)
-SYMBOL_PLAYER_1 = pg.transform.scale(pg.image.load("../../../images/X.png"), (SYMBOL_SIZE, SYMBOL_SIZE))
-SYMBOL_PLAYER_2 = pg.transform.scale(pg.image.load("../../../images/O.png"), (SYMBOL_SIZE, SYMBOL_SIZE))
+# SYMBOL_PLAYER_1 = pg.transform.scale(pg.image.load("../../../images/X.png"), (SYMBOL_SIZE, SYMBOL_SIZE))
+# SYMBOL_PLAYER_2 = pg.transform.scale(pg.image.load("../../../images/O.png"), (SYMBOL_SIZE, SYMBOL_SIZE))
+SYMBOL_PLAYER_1 = pg.transform.scale(pg.image.load("./images/X.png"), (SYMBOL_SIZE, SYMBOL_SIZE))
+SYMBOL_PLAYER_2 = pg.transform.scale(pg.image.load("./images/O.png"), (SYMBOL_SIZE, SYMBOL_SIZE))
 
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
